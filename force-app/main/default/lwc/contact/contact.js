fddeefc2f962b36d72fbe3b4c08a9dc3e6518071
Kevin Ha <kevincostnerha@gmail.com>
2021-12-09 07:17:22 -0800

    added button

--- force-app/main/default/lwc/contact/contact.js
+++ force-app/main/default/lwc/contact/contact.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import image from '@salesforce/resourceUrl/image';
 
-export default class Contact extends LightningElement {
+export default class Contact extends NavigationMixin (LightningElement) {
     plane = image + '/contactUs.svg';
     family = image + '/family.jpg';
     orange = image + '/size.png';
@@ -10,9 +10,9 @@ export default class Contact extends LightningElement {
     handleClick() {
         this[NavigationMixin.Navigate]({
             // Pass in pageReference
-            type: 'standard__namedPage',
+            type: 'standard__webPage',
             attributes: {
-                pageName: 'contact'
+                url: 'https://rentaspacep2-developer-edition.na163.force.com/s/contact'
             }
         });
     }
